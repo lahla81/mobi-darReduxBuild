21fa394f8612a1de14c107c9098df30a83b83199
lahla81 <lahla81@yahoo.com>
2019-05-03 05:10:36 +0200

    first commit

---
+++ precache-manifest.f083e0cd71c4bd1ae0249caac69ccfaf.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "900244d0afb74866d7626d9edd74a864",
+    "url": "/static/media/logo-ver.900244d0.jpg"
+  },
+  {
+    "revision": "b446a650f6cd6199abc4",
+    "url": "/static/css/main.91c91dad.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cb270cd068e427e91dad",
+    "url": "/static/js/2.ba2f843d.chunk.js"
+  },
+  {
+    "revision": "94fbd4e33b4e8774f661cabf8e8849ca",
+    "url": "/static/media/logo.94fbd4e3.jpg"
+  },
+  {
+    "revision": "e3db16600107110812494b6734a7c322",
+    "url": "/static/media/sony_logo.e3db1660.png"
+  },
+  {
+    "revision": "f57a6a516da9858f9ee97b1c7127e9f7",
+    "url": "/static/media/Samsung_Logo.f57a6a51.png"
+  },
+  {
+    "revision": "a603b57a8ce95ddffabc7138f8616385",
+    "url": "/static/media/Huawei-logo.a603b57a.png"
+  },
+  {
+    "revision": "cedc6f148097df2ce8b372d9787c4d50",
+    "url": "/static/media/LG-logo.cedc6f14.png"
+  },
+  {
+    "revision": "b446a650f6cd6199abc4",
+    "url": "/static/js/main.3d377949.chunk.js"
+  },
+  {
+    "revision": "28fb788ee6123377f5c93164e67eebd3",
+    "url": "/static/media/Cairo-Bold.28fb788e.ttf"
+  },
+  {
+    "revision": "882f12f7383f7b4be762259cc763fb64",
+    "url": "/static/media/Cairo-Regular.882f12f7.ttf"
+  },
+  {
+    "revision": "6e318a70bb258281a4ad71c3c2ce6b57",
+    "url": "/static/media/Cairo-SemiBold.6e318a70.ttf"
+  },
+  {
+    "revision": "b0938afa0f075ef4c30cb8d96a8def0b",
+    "url": "/static/media/icomoon.b0938afa.eot"
+  },
+  {
+    "revision": "74a306840ee62e46abf2d658cf30abd5",
+    "url": "/static/media/icomoon.74a30684.ttf"
+  },
+  {
+    "revision": "e89f7909c7d2619211f2ca0ec945925a",
+    "url": "/static/media/icomoon.e89f7909.woff"
+  },
+  {
+    "revision": "cb9d99abf39cba852ef1a3c5f46081f2",
+    "url": "/static/media/icomoon.cb9d99ab.svg"
+  },
+  {
+    "revision": "9c5064ae051d17e02d7e010d546fb10a",
+    "url": "/static/media/bg13.9c5064ae.png"
+  },
+  {
+    "revision": "057a52650408ea8648bc329df0a23361",
+    "url": "/index.html"
+  }
+];
